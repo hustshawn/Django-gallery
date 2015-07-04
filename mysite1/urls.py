5e6149865e4c52388bc75437a21988abb412ea91
hustshawn <hustshawn@gmail.com>
2015-07-04 15:39:08 +0800

    Some modifiation

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -11,5 +11,4 @@ urlpatterns = patterns('',
     url(r'^hello/$', 'trips.views.hello_world'),
     url(r'^post/(?P<id>\d+)/$', 'trips.views.post_detail',name='post_detail'),
 #	url(r'^hello/$', 'trips.views.hello_shawn'),
-
 )
--- trips/views.py
+++ trips/views.py
@@ -1,32 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
 # trips/views.py
-
-from django.http import HttpResponse
 from datetime import datetime
-from trips.models import Post
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Post
 
 def hello_world(request):
 #    return HttpResponse("Hello World!")
 	return	render(request,'hello_world.html',{'current_time':datetime.now()})
 
-def hello_shawn(request):
-	output = """
-	<!DOCTYPE html>
-	<html>
-		<head>
-			This is Head
-		</head>
-		<body>
-			<p> Hello Shawn ! <em style="color:LightSeaGreen;">{current_time} </em> </p>
-		</body>
-	</html>
-	""".format(current_time=datetime.now())
 
-	return HttpResponse(output)
 
 
 def home(request):
@@ -36,3 +20,18 @@ def home(request):
 def post_detail(request,id):
 	post = Post.objects.get(id=id)
 	return render(request, 'post.html',{'post':post})
+
+# def hello_shawn(request):
+# 	output = """
+# 	<!DOCTYPE html>
+# 	<html>
+# 		<head>
+# 			This is Head
+# 		</head>
+# 		<body>
+# 			<p> Hello Shawn ! <em style="color:LightSeaGreen;">{current_time} </em> </p>
+# 		</body>
+# 	</html>
+# 	""".format(current_time=datetime.now())
+
+# 	return HttpResponse(output)
